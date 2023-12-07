6d4856b9d02510dfc1fb9112208387033997b33e
Alissa Chimienti <alissachimienti@gmail.com>
2023-12-07 02:11:16 -0600

    deleted register

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -2,7 +2,6 @@ import CreateEntry from './Pages/CreateEntry.jsx';
 import MyDashboard from './Pages/MyDashboard.jsx';
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
-import Register from './Pages/Register.jsx';
 import ViewTopic from './Pages/ViewTopic.jsx';
 import CreateTopic from './Pages/CreateTopic.jsx';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
@@ -18,7 +17,6 @@ function App() {
           <Route exact path = '/createEntry' element = {<CreateEntry/>}/>
           <Route exact path = '/createTopic' element = {<CreateTopic/>}/>
           <Route exact path = '/viewTopic' element = {<ViewTopic/>}/>
-          <Route exact path = '/register' element = {<Register/>}/>
 
         </Routes>
       </div>
